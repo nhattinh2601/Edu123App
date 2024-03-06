756037462e3c226b39de75ca62de0c8e80c9ff26
nhattinh2601 <nhattinh2601@gmail.com>
2024-03-06 11:04:20 +0700

    home page with layout

--- src/component/Homepage.tsx
+++ src/component/Homepage.tsx
@@ -7,6 +7,7 @@ import {
   ScrollView,
   TouchableOpacity,
   StatusBar,
+  Dimensions,
 } from 'react-native';
 import {Rating} from 'react-native-ratings';
 import courseImage from '../image/cv-1_m.png';
@@ -23,69 +24,70 @@ import {
 
 function Course() {
   return (
-    <View
-      className="border-2 border-solid border-gray-300 w-50 h-50   rounded"
-      style={{width: 120, height: 120}}>
+    <View className="w-[40%] h-[100%] ml-3 mr-3  mt-0 mb-0  flex flex-col border-0 border-solid border-gray-300 w-50 h-50 bg-white  rounded items-center">
       <Image
+        className="w-full h-[40%]"
         source={courseImage} // Sử dụng đường dẫn tương đối
-        style={{width: 96, height: 54, borderRadius: 8}}
       />
-
-      <Text style={{fontSize: 10, textAlign: 'center'}}>
-        Thành thạo Canvas trong 21 ngày
+      <Text className="text-base font-bold  text-black" style={{alignSelf: 'flex-start'}}>
+        Thành thạo Canvas trong 21 ngày 
       </Text>
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+      <Text style={{alignSelf: 'flex-start'}}> Nguyễn Nhật Tính </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          alignSelf: 'flex-start',
+        }}>
         <Rating ratingCount={5} imageSize={15} startingValue={4} />
+        <Text> (10)</Text>
       </View>
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-        <Text style={{fontSize: 10}}>298.000đ </Text>
-        <Text style={{fontSize: 10, color: 'gray'}}>500.000đ</Text>
+
+      <View style={{flexDirection: 'row', justifyContent: 'center', alignSelf: 'flex-start'}} >
+        <Text className="text-base font-bold" >298.000đ </Text>
+        <Text className="text-sm line-through">500.000đ</Text>
       </View>
     </View>
   );
 }
-
-function CourseNew() {
-  return (
-    <View className="w-[30%] h-[90%]  m-3 flex flex-col border-2 border-solid border-gray-300 w-50 h-50 bg-white  rounded">
-      <Image
-        className=""
-        source={courseImage} // Sử dụng đường dẫn tương đối
-        style={{width: 96, height: 54, borderRadius: 8}}
-      />
-      <Text style={{fontSize: 10, textAlign: 'center'}}>
-        Thành thạo Canvas trong 21 ngày
-      </Text>
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-        <Rating ratingCount={5} imageSize={15} startingValue={4} />
-      </View>
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-        <Text style={{fontSize: 10}}>298.000đ </Text>
-        <Text style={{fontSize: 10, color: 'gray'}}>500.000đ</Text>
-      </View>
-    </View>
-  );
-}
-
+const {height} = Dimensions.get('window');
 export default function HomePage() {
   return (
     <View className="flex flex-col justify-center items-center">
-      <ScrollView>
-      <View className="w-[90%] h-[30%] bg-white m-3 flex flex-row justify-center items-center">
-      <Image
-        className="w-[90%] h-[90%]"
+      <ScrollView className="w-full h-[94%] ">
+      <View
+          className="w-full  bg-green-200 ml-2 mr-2  mt-0 mb-0 flex flex-row justify-center items-center"
+          style={{height: 0.25 * height}}>
+            <Image
+        className="w-full h-full" resizeMode="stretch"
         source={slideshow1} // Sử dụng đường dẫn tương đối
       />
-      </View>
-    <View className="w-[90%] h-[50%] bg-slate-300">
-      <View className="w-[90%] h-[30%] bg-green-200 m-3 flex flex-row">
-        <CourseNew />
-        <View className="w-[30%] h-[90%] bg-blue-200 m-3 flex flex-col"></View>
-        <View className="w-[30%] h-[90%] bg-blue-200 m-3 flex flex-col"></View>
-      </View>
-      </View>
+          </View>
+          <Text className="text-xl font-bold ml-5">Top bán chạy</Text>
+        <View
+          className="w-full  m-0 p-0 border-0 flex flex-row "
+          style={{height: 0.25 * height}}>
+          <Course /> 
+          <Course />
+          <Course />
+        </View>
+        <Text className="text-xl font-bold ml-5">Đang khuyến mãi</Text>
+        <View
+          className="w-full  m-0 p-0 border-0 flex flex-row "
+          style={{height: 0.25 * height}}>
+          <Course /> 
+          <Course />
+          <Course />
+        </View>
+        <Text className="text-xl font-bold ml-5">Học nhiều</Text>
+        <View
+          className="w-full  m-0 p-0 border-0 flex flex-row "
+          style={{height: 0.25 * height}}>
+          <Course /> 
+          <Course />
+          <Course />
+        </View>
       </ScrollView>
-
     </View>
   );
 }
